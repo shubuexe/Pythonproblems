2b5251c311e2015be151c9abc54974d678e455ff
Shubham <sp955219@gmail.com>
2025-06-20 13:33:18 +0530

    Add files via upload

---
+++ Chapter 2/04_operators.py
@@ -0,0 +1,37 @@
+#arithmetic operators
+a=7
+b=89
+c=7
+d= a+b-c
+print (d)
+
+#assignmemnt operators
+a = 5-7 #assign 4-2 in a
+print(a)
+b = 6 
+
+b-= 3 #dcrment the value of b by three then asign it to b
+print(b)
+
+#comparison operators
+d= 5>8
+print (d)
+
+c=4>=6     #greater than or equal to
+print (c)
+
+x=4!=8     #not equal to
+print (x)
+
+w= 5==4  # equal to
+print (w)
+
+#logical operators or,and, nand operators uses truth table to determine whether true or false
+e = True or False
+print(e)
+
+f = True and False
+print (f)
+ 
+v = (not(False))
+print (v)
---
+++ Chapter 2/06_input.py
@@ -0,0 +1,21 @@
+a = input ("Enter number 1:")
+b = input ("Enter number 2:")
+
+print ("Number a is:",a)
+print ("Number b is:",b)
+
+#concatination
+a = input ("Enter number 1:")
+b = input ("Enter number 2:")
+
+print ("Number a is:",a)
+print ("Number b is:",b)
+print("sum is", a+b)   
+
+#addition
+a = int(input ("Enter number 1:"))
+b = int(input ("Enter number 2:"))
+
+print ("Number a is:",a)
+print ("Number b is:",b)
+print("sum is", a+b)  
