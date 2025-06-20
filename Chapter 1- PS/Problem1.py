65fee19b479fbbc39c1863323ba274368c15b3b4
Shubham <sp955219@gmail.com>
2025-06-20 11:53:40 +0530

    Update Problem1.py

--- Chapter 1- PS/Problem1.py
+++ Chapter 1- PS/Problem1.py
@@ -1,13 +1,29 @@
-print('''Twinkle twinkle little star.
-How I wonder what you are.
-Up above the world so high.
-Like a diamond in the sky.
-Twinkle twinkle little star.
-How I wonder what you are.
+print('''Good, better, best,
+Never let it rest,
+Till your good is better,
+And your better is best.
 
-Twinkle twinkle little star.
-How I wonder what you are.
-Up above the world so high.
-Like a diamond in the sky.
-Twinkle twinkle little star.
-How I wonder what you are.''')
+Wake up with a smile,
+Try and go the extra mile.
+Even if the task feels tough,
+Trying hard is good enough.
+
+Learn from every fall,
+Stand up strong and tall.
+Mistakes are how we grow,
+Much more than we know.
+
+Be kind in what you do,
+Help your classmates too.
+A gentle word, a cheerful face,
+Can brighten up the place.
+
+Practice every day,
+Even when you want to play.
+Hard work makes you smart,
+So give it all your heart.
+
+Good, better, best,
+Always do your best,
+With courage in your chest,
+You’ll stand out from the rest!''')
